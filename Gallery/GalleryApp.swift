b011a04e6ce5b4331e770b6a90eba2ef384a82f3
Ibe Vangheluwe <ibe.vangheluwe@iMacL.local>
2025-12-04 11:51:03 +0100

    first commit

---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Ibe Vangheluwe on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
